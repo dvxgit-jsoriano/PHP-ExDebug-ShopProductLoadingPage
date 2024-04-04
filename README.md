cb8a3a3373b22b04ac0148674a89cac8e0a8e7cb
dvxgit-jsoriano <jsoriano@diavox.net>
2024-04-04 15:43:41 +0800

    Simple Program with some errors/bugs to fix.

--- README.md
+++ README.md
@@ -2,3 +2,17 @@
 
 This is a PHP practical exercise where you have to debug the program to make it work. Find as many bugs as possible. Find some hints from the README.md
 
+## Actual Website Display
+
+![Website Display](images/image.png)
+
+## Bug Find and Fix
+
+- There are about 6 bugs in this simple program. Find them and fix the errors and bugs.
+- Notes the bugs you found and solution you applied.
+
+| ---------- | ------------|
+| Bugs Found | Solutions   |
+| ---------- | ------------|
+| No comma   | Added comma |
+| ---------- | ------------|
--- index.php
+++ index.php
@@ -1,11 +1,46 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Shop Product Loading Page</title>
+    <title>PHP-ExDebug-ShopProductLoadingPage</title>
 </head>
+
 <body>
-    
+    <div class="mb-4">
+        <h1 class="text-xl text-red-800 bg-red-100 text-center p-2">PHP-ExDebug-ShopProductLoadingPage</h1>
+    </div>
+
+    <div class="flex flex-wrap gap-4 justify-center">
+        <?php
+        $products = file_get_contents('products.json');
+        //var_dump($products); // You can use var_dump method to debug. It is very useful
+        foreach ($products as $product) {
+            // Heredoc string
+            echo <<<<PRODUCT
+            <div class="max-w-sm rounded-xl overflow-hidden shadow-lg w-1/4 p-4">
+                <img class="w-full p-12" src="$product->image_url" alt="Coke">
+                <div class="px-6 py-4">
+                    <div class="font-bold text-xl mb-2">$product->name</div>
+                    <p class="text-gray-700 text-base">
+                        Category: $product->main_category <br>
+                        Sub Category: $product->sub_category <br>
+                        Quantity: $product->quantity <br>
+                        Price: PHP $products->price_php
+                    </p>
+                </div>
+                <div class="px-6 pt-4 pb-2">
+                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#Beverages</span>
+                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#SoftDrinks</span>
+                </div>
+            </div>
+        PRODUCT;
+        }
+        ?>
+    </div>
+
+    <script src="https://cdn.tailwindcss.com"></script>
 </body>
+
 </html>
